7512581b46a29ad4bb46e8fbaa45cbf6b0cb4a7c
jere168 <115753433+jere168@users.noreply.github.com>
2022-10-29 23:26:49 +0700

    Add files via upload

---
+++ tbreceh.js
@@ -0,0 +1,9 @@
+const fetch = require('isomorphic-fetch');
+const params = {
+  apiKey: "0BQku3j3SWQkvkCT-R9xrsyME38XLcxEmxgDYR_Ph1zIXSkzQXiIAwCprcs",
+  spreadsheetId: "12MVkXejBeXEPCN8kEQ-tglQu6Z93C2w-921_RvVvoHM"
+}
+const url = = new URL("https://api.sheetson.com/v2/sheets/tbreceh");
+Object.keys(params).forEach(key => url.searchParams.append(key, encodeURIComponent(params[key])));
+fetch(url).then(r => r.json())
+  .then(result => console.log(result))
